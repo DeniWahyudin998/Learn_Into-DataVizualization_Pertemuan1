55d27f4ff4bdb50b12949b81930c1c8a31f56230
unknown <deniwahyudin998@gmail.com>
2022-06-11 14:31:13 +0700

    Initial Commit add Sesi 1

---
+++ Latihan Pertemuan Pertama.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 11:33:02 2022
+
+@author: 081796
+"""
+
+a = 10
+t = 20
+l = 1*(a*t)/2
+
+print(l)
---
+++ Pertemuan Pertama.py
@@ -0,0 +1,114 @@
+##Integers
+print(123123123123123123123123123123123123123123123123 + 1)
+
+print(10)
+print(type(10))
+
+##Floating-Point Numbers
+print(4.2)
+print(type(4.2))
+print(4.)
+print(.2)
+print(.4e7)
+print(4.2e-4)
+
+##Strings
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+print("This string contains a single quote (') character.")
+print('This string contains a double quote (") character.')
+
+##Boolean Type, Boolean Context, and “Truthiness”
+print(type(True))
+print(type(False))
+
+##Variable Assignment
+n = 1000
+print(n)
+n
+
+a = b = c = 300
+print(a, b, c)
+
+##Variable Types in Python
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+##Variable Names
+name = "Hacktiv8"
+Age = 54
+has_laptops = True
+print(name, Age, has_laptops)
+
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+
+print(age, Age, aGe, AGE, a_g_e, _age, age_, _AGE_)
+
+a = 10
+b = 20
+a + b
+
+##Operators and Expressions in Python
+print(a + b)
+
+a = 10
+b = 20
+a + b - 5
+print(a + b - 5)
+
+##Arithmetic Operators
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b) ##fungsi mode / mod / sisa bagi
+print(a ** b) ##fungsi kuadrat
+
+
+##Comparison Operators
+
+a = 10
+b = 20
+print(a == b)
+
+print(a != b)
+
+print(a <= b)
+
+print(a >= b)
+
+
+a = 30
+b = 30
+print(a == b)
+
+print(a <= b)
+
+print(a >= b)
+
+##String Manipulation
+
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8 ' + 'PTP')
